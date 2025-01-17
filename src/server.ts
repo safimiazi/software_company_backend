0e42133632139890bf5bdbfdfa86bb72b57482cb
safi miazi <mohibullamiazi@gmail.com>
2025-01-17 21:01:45 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL);
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
