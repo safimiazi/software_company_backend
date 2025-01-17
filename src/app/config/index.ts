ebc6ade910aa0a2569aa26f375262f9d08388449
safi miazi <mohibullamiazi@gmail.com>
2025-01-17 23:06:29 +0600

    update

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv'
-import path from "path"
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.join(process.cwd() , ".env") })
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    PORT:process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL
-}
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
