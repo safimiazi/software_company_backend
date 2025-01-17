56f2253c41c855d5459c28f465100bac85ff9d41
safi miazi <mohibullamiazi@gmail.com>
2025-01-17 21:37:17 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,14 @@ import config from "./app/config";
 import mongoose from "mongoose";
 
 async function main() {
-  await mongoose.connect(config.DATABASE_URL as string);
-  app.listen(config.PORT, () => {
-    console.log(`Example app listening on port ${config.PORT}`);
-  });
+  try {
+    await mongoose.connect(config.DATABASE_URL as string);
+    app.listen(config.PORT, () => {
+      console.log(`Example app listening on port ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-main()
+main();
