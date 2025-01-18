e2032c2d45a6d076a6f04b7c272d8c97e8b59e90
safi miazi <mohibullamiazi@gmail.com>
2025-01-18 23:47:18 +0600

    update

---
+++ src/app/modules/admin/admin.model.ts
@@ -0,0 +1,34 @@
+import { Schema } from "mongoose";
+import { Admin } from "./admin.interface";
+
+const adminSchema = new Schema<Admin>({
+
+  id: {
+    type: String,
+
+  },
+  name: {
+    type: String,
+    required:true
+  },
+  email: {
+    type: String,
+    required:true
+
+  },
+  phone: {
+    type: String,
+
+  },
+  picture: {
+    type: String,
+
+  },
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+
+});
