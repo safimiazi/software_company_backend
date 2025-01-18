114eecc77eb6fedbf18ccf17bca514cf4a2e53d7
safi miazi <mohibullamiazi@gmail.com>
2025-01-19 00:22:36 +0600

    update

--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -1,38 +1,39 @@
 import { model, Schema } from "mongoose";
-import {  Admin } from "./admin.interface";
+import { Admin } from "./admin.interface";
 
 const adminSchema = new Schema<Admin>({
-
   id: {
     type: String,
-    unique: true
-
+    unique: true,
   },
   name: {
     type: String,
-    required:true
+    required: [true, "First Name is required"],
+    maxlength: [20, "Name can not be more than 20 characters"],
+    validate: {
+      validator: function (value: string) {
+        const name = value.charAt(0).toUpperCase() + value.slice(1);
+        return value === name;
+      },
+      message: "{VALUE} is not capitalized format.",
+    },
   },
   email: {
     type: String,
-    required:true
-
+    required: true,
   },
   phone: {
     type: String,
-
   },
   picture: {
     type: String,
-
   },
   createdAt: {
-    type: Date
+    type: Date,
   },
   updatedAt: {
-    type: Date
+    type: Date,
   },
-
 });
 
-
-export const adminModel = model<Admin>('Admin', adminSchema);
+export const adminModel = model<Admin>("Admin", adminSchema);
