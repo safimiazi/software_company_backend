e68bc415106cca1d68ae5fc6fbb33a5fcc92421f
safi miazi <mohibullamiazi@gmail.com>
2025-01-19 23:30:55 +0600

    update

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -1,19 +1,16 @@
 export type TAdmin = {
-    name: string; // Admin's full name
-    
-    email: string;
-    password: string;
-    needsPasswordChange: boolean;
-    passwordChangeAt?: Date;
-    role: 'superAdmin' | 'admin'| 'employee';
-    status: 'active'| 'blocked';
-    isDeleted: boolean;
-    
-    phone?: string; // Optional phone number
-    picture?: string; // Optional profile picture URL
-    createdAt?: Date; // Creation date (optional, auto-generated)
-    updatedAt?: Date; // Update date (optional, auto-generated)
-  
-  
-  };
-  
+  name: string;
+
+  email: string;
+  password: string;
+  needsPasswordChange: boolean;
+  passwordChangeAt?: Date;
+  role: "superAdmin" | "admin" | "employee";
+  status: "active" | "blocked";
+  isDeleted: boolean;
+
+  phone: string;
+  picture: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
