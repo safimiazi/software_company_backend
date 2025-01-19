b61d7010014347aa3809d2a0de77957b360d5ffc
safi miazi <mohibullamiazi@gmail.com>
2025-01-19 22:57:40 +0600

    :upda

---
+++ src/app/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { adminRoutes } from "../modules/admin/admin.routes";
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/admin",
+    route: adminRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+export default router;
