d45fca660e9a783d0c9ab9e6b09065c2dccf7f47
safi miazi <mohibullamiazi@gmail.com>
2025-01-20 17:37:50 +0600

    update

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,31 +1,23 @@
-import { NextFunction, Request, Response } from "express";
 import { adminServices } from "./admin.service";
 import sendResponse from "../../utils/sendResponse";
 import status from "http-status";
+import catchAsync from "../../utils/catchAsync";
 
-const create_admin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    // Extract data from request body
-    const data = req.body;
+const create_admin = catchAsync(async (req, res) => {
+  // Extract data from request body
+  const data = req.body;
 
-    // Register the admin in the database
-    const result = await adminServices.create_admin_into_db(data);
+  // Register the admin in the database
+  const result = await adminServices.create_admin_into_db(data);
 
-    // Send a success response
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admin is created succesfully.",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  // Send a success response
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admin is created succesfully.",
+    data: result,
+  });
+});
 
 export const adminControllers = {
   create_admin,
