d3df5fd43a97bec6a8704debb3605224dbafc12d
safi miazi <mohibullamiazi@gmail.com>
2025-01-20 14:48:05 +0600

    update global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express, { Application, Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import router from "./app/routes";
@@ -6,12 +8,20 @@ import router from "./app/routes";
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1', router)
-
-
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = 500;
+  const meassage = err.meassage || "Something went wrong.";
+  res.status(statusCode).json({
+    success: false,
+    meassage,
+    error: err,
+  });
 });
 
 export default app;
