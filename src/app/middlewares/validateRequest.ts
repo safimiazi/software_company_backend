5a3da263dbfc3ed89eb2bbfec5b090f317d8a6fd
safi miazi <mohibullamiazi@gmail.com>
2025-01-27 22:03:09 +0600

    fix: streamline request body parsing in validateRequest middleware

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -6,9 +6,7 @@ import { AnyZodObject } from "zod";
 
 export const validateRequest = (schema: AnyZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    await schema.parseAsync(
-    req.body,
-    );
+    await schema.parseAsync(req.body);
 
     next();
   });
