e983b057438252e72e7917b8bc877726a28ec87f
safi miazi <mohibullamiazi@gmail.com>
2025-01-28 23:17:31 +0600

    fix: enhance global error handler to include structured error source information

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import express, { Application, ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import express, {
+  Application,
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import status from "http-status";
 
 const globalErrorHandler: ErrorRequestHandler = (
@@ -9,14 +15,25 @@ const globalErrorHandler: ErrorRequestHandler = (
   res,
   next: NextFunction
 ) => {
-
-
   const statusCode = status.INTERNAL_SERVER_ERROR;
   const meassage = err?.message || "Something went wrong.";
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  };
+
+  const errorSource: TErrorSource[] = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
   res.status(statusCode).json({
     success: false,
     meassage,
-    error: err,
+    errorSource,
   });
 };
 
