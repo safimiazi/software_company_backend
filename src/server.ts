b88554ac38e35792cca1b976f756c41ef3254682
safi miazi <mohibullamiazi@gmail.com>
2025-01-29 23:43:25 +0600

    feat: implement server shutdown handling for unhandled rejections and uncaught exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
+import { Server } from "http";
 import app from "./app";
 import config from "./app/config";
 
 import mongoose from "mongoose";
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(config.DATABASE_URL as string);
-    app.listen(config.PORT, () => {
+    server = app.listen(config.PORT, () => {
       console.log(`Example app listening on port ${config.PORT}`);
     });
   } catch (error) {
@@ -15,3 +18,18 @@ async function main() {
 }
 
 main();
+
+process.on("unhandledRejection", (err) => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
