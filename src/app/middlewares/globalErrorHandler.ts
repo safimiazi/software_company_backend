46e6bffd18f709820d21cdfeaaf3144963ee282c
safi miazi <mohibullamiazi@gmail.com>
2025-01-29 22:42:24 +0600

    feat: enhance global error handler to include AppError handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -15,6 +15,7 @@ import handleZodError from "../errors/handleZodError";
 import handleValidationError from "../errors/handleValidationError";
 import handleCastError from "../errors/handleCastError";
 import handleDuplicateError from "../errors/handleDuplicateError";
+import AppError from "../errors/AppError";
 
 const globalErrorHandler: ErrorRequestHandler = (
   err,
@@ -23,7 +24,7 @@ const globalErrorHandler: ErrorRequestHandler = (
   next: NextFunction
 ) => {
   let statusCode: string | number = status.INTERNAL_SERVER_ERROR;
-  let meassage = err?.message || "Something went wrong.";
+  let meassage = "Something went wrong.";
   let errorSource: TErrorSource[] = [
     {
       path: "",
@@ -31,36 +32,46 @@ const globalErrorHandler: ErrorRequestHandler = (
     },
   ];
 
-
-// handle zod error
+  // handle zod error
   if (err instanceof ZodError) {
     const simplifiedErrors = handleZodError(err);
     statusCode = simplifiedErrors?.statusCode;
     meassage = simplifiedErrors?.message;
     errorSource = simplifiedErrors?.errorSource;
-  }
-  else if (err?.name === 'ValidationError') {
+  } else if (err?.name === "ValidationError") {
     const simplifiedError = handleValidationError(err);
     statusCode = simplifiedError?.statusCode;
     meassage = simplifiedError?.message;
     errorSource = simplifiedError?.errorSource;
-  }
-  else if (err?.name === "CastError") {
+  } else if (err?.name === "CastError") {
     const simplifiedError = handleCastError(err);
     statusCode = simplifiedError?.statusCode;
     meassage = simplifiedError?.message;
     errorSource = simplifiedError?.errorSource;
-  }
-  else if (err?.code === "11000") {
+  } else if (err?.code === "11000") {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     meassage = simplifiedError?.message;
     errorSource = simplifiedError?.errorSource;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    meassage = err.message;
+    errorSource = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    meassage = err.message;
+    errorSource = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
   }
 
-
-
-  
   res.status(statusCode).json({
     success: false,
     meassage,
