72157e38ab09521c4e231508aab9ff58aeaf79fa
safi miazi <mohibullamiazi@gmail.com>
2025-01-31 23:45:48 +0600

    feat: update admin login route to use specific validation schema

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { adminControllers } from "./admin.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { adminValidationSchema } from "./admin.validation";
+import { adminLoginValidationSchema, adminValidationSchema } from "./admin.validation";
 const router = express.Router();
 
 router.post("/create", validateRequest(adminValidationSchema), adminControllers.create_admin);
-router.post("/login", validateRequest(adminValidationSchema), adminControllers.login_admin)
+router.post("/login", validateRequest(adminLoginValidationSchema), adminControllers.login_admin)
 export const adminRoutes = router;
