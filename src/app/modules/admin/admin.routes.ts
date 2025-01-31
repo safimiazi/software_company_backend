6fe910db950085c015994b545c116f65f69113e9
safi miazi <mohibullamiazi@gmail.com>
2025-01-31 23:15:34 +0600

    feat: add login route to admin routes

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -5,5 +5,5 @@ import { adminValidationSchema } from "./admin.validation";
 const router = express.Router();
 
 router.post("/create", validateRequest(adminValidationSchema), adminControllers.create_admin);
-
+router.post("/login", )
 export const adminRoutes = router;
