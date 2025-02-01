4f89b16162c1d859d8aadf1493949fa1277ceaba
safi miazi <mohibullamiazi@gmail.com>
2025-02-01 22:50:01 +0600

    feat: integrate cookie-parser and set access token in admin login response

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 // parser
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
 
 app.use("/api/v1", router);
 
--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -2,6 +2,7 @@ import { adminServices } from "./admin.service";
 import sendResponse from "../../utils/sendResponse";
 import status from "http-status";
 import catchAsync from "../../utils/catchAsync";
+import config from "../../config";
 
 const create_admin = catchAsync(async (req, res) => {
   // Extract data from request body
@@ -22,13 +23,20 @@ const create_admin = catchAsync(async (req, res) => {
 const login_admin = catchAsync(async (req, res) => {
   const data = req.body;
 
-  const result = await adminServices.login_admin_into_db(data);
+  const access_token = await adminServices.login_admin_into_db(data);
+
+  res.cookie("access_token", access_token, {
+    httpOnly: true,
+    secure: config.node_env === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  });
 
   sendResponse(res, {
     statusCode: status.OK,
     success: true,
     message: "Admin is login successfully.",
-    data: result,
+    data: {},
   });
 });
 
