28056cde11491e5975c885c9f82c060a130d6dea
safi miazi <mohibullamiazi@gmail.com>
2025-02-02 23:49:05 +0600

    style: improve formatting and readability in admin model

--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -9,7 +9,7 @@ import status from "http-status";
 const adminSchema = new Schema<TAdmin, TAdminModel>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true , select: 0},
+  password: { type: String, required: true, select: 0 },
   needsPasswordChange: { type: Boolean, default: false },
   passwordChangeAt: { type: Date },
   role: { type: String, enum: ["admin", "employee"], default: "employee" },
@@ -35,8 +35,6 @@ adminSchema.post("save", async function (doc, next) {
   next();
 });
 
-
-
 // check if admin already exists
 adminSchema.pre("save", async function (next) {
   const result = await adminModel.findOne({ email: this.email });
@@ -46,9 +44,8 @@ adminSchema.pre("save", async function (next) {
   next();
 });
 
-
 adminSchema.statics.isAdminExistById = async function (id: string) {
-  return await adminModel.findOne({_id: id})
-}
+  return await adminModel.findOne({ _id: id });
+};
 
 export const adminModel = model<TAdmin, TAdminModel>("Admin", adminSchema);
