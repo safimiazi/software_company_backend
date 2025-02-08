16eb81943052c36bac40e8ded6b32719278f2840
safi miazi <mohibullamiazi@gmail.com>
2025-02-08 20:18:07 +0600

    feat: enhance CORS configuration to allow specific origins and methods

--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin:'*', 
-    credentials: true, // Important for cookies/session
-  })
-);
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+
+app.options('*', cors({
+  origin: (origin, callback) => {
+    console.log("Preflight Request Origin:", origin);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
+}));
+
+
 
 // app.post('/track', (req, res) => {
 //   const { url, timestamp, childId } = req.body;
