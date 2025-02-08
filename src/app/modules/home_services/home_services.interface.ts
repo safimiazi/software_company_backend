52f3765907d5cabbe03b45dc1df62841031dbc76
safi miazi <mohibullamiazi@gmail.com>
2025-02-08 20:38:44 +0600

    feat: add home_services interface and model with validation for service properties

--- src/app/modules/home_services/home_services.interface.ts
+++ src/app/modules/home_services/home_services.interface.ts
@@ -1,1 +1,17 @@
 // home_services.interface.ts - home_services module
+// Define TypeScript Interface
+interface IService {
+  title: string;
+  description: string;
+  ctaText: string;
+  ctaLink: string;
+}
+
+export interface IServices {
+  title: string;
+  description: string;
+  heading: string;
+  ctaText: string;
+  ctaLink: string;
+  services: IService[];
+}
---
+++ src/app/modules/home_services/home_services.model.ts
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+import { IServices } from "./home_services.interface";
+
+// Define the Mongoose Schema
+const ServiceSchema: Schema = new Schema<IServices>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      minlength: [3, "Title must be at least 3 characters"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    heading: {
+      type: String,
+      required: [true, "Heading is required"],
+      trim: true,
+    },
+    ctaText: {
+      type: String,
+      required: [true, "CTA text is required"],
+      trim: true,
+    },
+    ctaLink: {
+      type: String,
+      required: [true, "CTA Link is required"],
+    },
+    services: [
+      {
+        title: {
+          type: String,
+          required: [true, "Service Title is required"],
+          minlength: [3, "Service Title must be at least 3 characters"],
+          trim: true,
+        },
+        description: {
+          type: String,
+          required: [true, "Service Description is required"],
+          trim: true,
+        },
+        ctaText: {
+          type: String,
+          required: [true, "Service CTA text is required"],
+          trim: true,
+        },
+        ctaLink: {
+          type: String,
+          required: [true, "Service CTA Link is required"],
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Create & Export the Model
+const ServiceModel = mongoose.model<IServices>("Service", ServiceSchema);
+export default ServiceModel;
