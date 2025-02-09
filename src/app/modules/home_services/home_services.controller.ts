df09d91975c76533c68eb4839ac5cdaabed7ffcd
safi miazi <mohibullamiazi@gmail.com>
2025-02-09 22:26:45 +0600

    feat: refactor admin_post_Services function for improved readability and structure

--- src/app/modules/home_services/home_services.controller.ts
+++ src/app/modules/home_services/home_services.controller.ts
@@ -6,19 +6,18 @@ import { services_db } from "./home_services.service";
 
 // home_services.controller.ts - home_services module
 const admin_post_Services = catchAsync(
-    async (req: IServiceRequestWithFile, res) => {
-        
-      const result = await services_db.admin_post_services_into_db(req.body);
-  
-      sendResponse(res, {
-        statusCode: status.OK,
-        success: true,
-        message: "Service is saved successfully.",
-        data: result,
-      });
-    }
-  );
+  async (req: IServiceRequestWithFile, res) => {
+    const result = await services_db.admin_post_services_into_db(req.body);
 
-  export const serviceController = {
-    admin_post_Services
+    sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: "Service is saved successfully.",
+      data: result,
+    });
   }
+);
+
+export const serviceController = {
+  admin_post_Services,
+};
