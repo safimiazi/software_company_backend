7923aaa9057c2f592a30f65796c20a3433d8fa34
safi miazi <mohibullamiazi@gmail.com>
2025-02-09 22:53:19 +0600

    feat: update PUT service route to use servicePutValidationSchema for improved validation

--- src/app/modules/home_services/home_services.routes.ts
+++ src/app/modules/home_services/home_services.routes.ts
@@ -2,7 +2,7 @@
 import express from "express";
 import path from "path";
 import { getMuler } from "../../middlewares/multer";
-import { serviceValidationSchema } from "./home_services.validation";
+import { servicePutValidationSchema, serviceValidationSchema } from "./home_services.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { serviceController } from "./home_services.controller";
 const router = express.Router();
@@ -17,7 +17,7 @@ const upload = getMuler({
 
 
 router.post("/post_services_data", upload.single("image"),validateRequest(serviceValidationSchema), serviceController.admin_post_Services)
-router.put("/put_services_data/:id", upload.single("image"),validateRequest(serviceValidationSchema) , serviceController.admin_put_Services)
+router.put("/put_services_data/:id", upload.single("image"),validateRequest(servicePutValidationSchema) , serviceController.admin_put_Services)
 router.delete("/delete_services_data/:id" )
 router.get("/get_services_data")
 
