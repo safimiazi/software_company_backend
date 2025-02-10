70acdbde86423c5ccfad3719ec82d99711cfcce9
safi miazi <mohibullamiazi@gmail.com>
2025-02-10 23:47:31 +0600

    feat: add support for CORS credentials in app configuration

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ app.options('*', cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
+  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
