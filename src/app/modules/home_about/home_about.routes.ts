107b3afd80c75f47b5141e2e4a74cbc7358ed488
safi miazi <mohibullamiazi@gmail.com>
2025-02-13 21:37:32 +0600

    feat: update home_about routes to use uploadService, streamline route definitions, and improve code organization

--- src/app/modules/home_about/home_about.routes.ts
+++ src/app/modules/home_about/home_about.routes.ts
@@ -1,28 +1,40 @@
 // home_about.routes.ts - home_about module
 import express from "express";
-import path from "path";
-import {  getMuler } from "../../middlewares/multer";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { photoComposure } from "../../middlewares/photoComposure";
 import { HomeAboutValidationSchema } from "./home_about.validation";
 import { homeAboutControllers } from "./home_about.controller";
+import { uploadService } from "../upload/upload";
 
 const router = express.Router();
 
-const upload_file_destination_path = path.join(__dirname, "../../upload_files");
-
-const upload = getMuler({
-  upload_file_destination_path,
-  regex: /jpeg|jpg|png|pdf/,
-  images: "jpg, jpeg, png, pdf",
-});
 const { configurableCompression } = photoComposure();
 
+router.post(
+  "/post_home_about_data",
+  validateRequest(HomeAboutValidationSchema),
+  uploadService.single("image"),
+  configurableCompression("jpeg", 60),
+  homeAboutControllers.admin_post_home_about
+);
+router.put(
+  "/put_home_about_data/:id",
+  uploadService.single("image"),
+  configurableCompression("jpeg", 60),
+  validateRequest(HomeAboutValidationSchema),
+  homeAboutControllers.admin_put_home_about
+);
+router.delete(
+  "/delete_home_about_data/:id",
+  homeAboutControllers.admin_delete_home_about
+);
+router.get(
+  "/get_home_page_about_data",
+  homeAboutControllers.get_home_about_data
+);
+router.get(
+  "/get_home_page_about_images/:id",
+  homeAboutControllers.get_home_about_images
+);
 
-router.post("/post_home_about_data",validateRequest(HomeAboutValidationSchema) ,upload.single("image"),   configurableCompression("jpeg", 60), homeAboutControllers.admin_post_home_about )
-router.put("/put_home_about_data/:id", upload.single("image"),   configurableCompression("jpeg", 60),validateRequest(HomeAboutValidationSchema), homeAboutControllers.admin_put_home_about )
-router.delete("/delete_home_about_data/:id", homeAboutControllers.admin_delete_home_about )
-router.get("/get_home_page_about_data", homeAboutControllers.get_home_about_data )
-router.get("/get_home_page_about_images/:id", homeAboutControllers.get_home_about_images )
-
- export const homeAboutRoutes = router;
+export const homeAboutRoutes = router;
