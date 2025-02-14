aecd7efd20c9c12d2823b5e6fe30f9b0a91825ed
safi miazi <mohibullamiazi@gmail.com>
2025-02-14 12:10:27 +0600

    feat: add a new line for improved readability in home_services controller

--- src/app/modules/home_services/home_services.controller.ts
+++ src/app/modules/home_services/home_services.controller.ts
@@ -7,6 +7,7 @@ import { services_db } from "./home_services.service";
 // home_services.controller.ts - home_services module
 const admin_post_Services = catchAsync(
   async (req: IServiceRequestWithFile, res) => {
+    
     const result = await services_db.admin_post_services_into_db(req.body);
 
     sendResponse(res, {
