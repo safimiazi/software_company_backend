e2f7a92b1768ef9deeddf484662d4877b1ccd83e
safi miazi <mohibullamiazi@gmail.com>
2025-02-14 12:03:59 +0600

    feat: refactor home_services routes to use centralized upload service for image handling

--- src/app/modules/home_services/home_services.routes.ts
+++ src/app/modules/home_services/home_services.routes.ts
@@ -1,32 +1,25 @@
 // home_services.routes.ts - home_services module
 import express from "express";
-import path from "path";
-import { getMuler } from "../../middlewares/multer";
 import {
   servicePutValidationSchema,
   serviceValidationSchema,
 } from "./home_services.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { serviceController } from "./home_services.controller";
+import { uploadService } from "../upload/upload";
 const router = express.Router();
 
-const upload_file_destination_path = path.join(__dirname, "../../upload_files");
 
-const upload = getMuler({
-  upload_file_destination_path,
-  regex: /svg/,
-  images: "svg",
-});
 
 router.post(
   "/post_services_data",
-  upload.single("image"),
+  uploadService.single("image"),
   validateRequest(serviceValidationSchema),
   serviceController.admin_post_Services
 );
 router.put(
   "/put_services_data/:id",
-  upload.single("image"),
+  uploadService.single("image"),
   validateRequest(servicePutValidationSchema),
   serviceController.admin_put_Services
 );
